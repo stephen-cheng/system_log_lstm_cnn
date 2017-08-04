a8f2c86bf44e16d6d6afadcedc13f09c33a2e9ce
jetrobert <jetrobert@live.com>
2017-08-04 19:57:43 +0800

    new

--- code/train_predict/train_test_partition.py
+++ code/train_predict/train_test_partition.py
@@ -1,4 +1,13 @@
 
+def data_even(raw_file, new_file):
+  with open('data/'+raw_file, 'r') as rf:
+    with open('data/'+new_file, 'w+') as wf:
+      line_num = 0
+      for line in rf.readlines():
+        line_num += 1
+        if line_num <= 10000 or line_num >=53218:
+          wf.write(line.strip()+'\n')
+
 def train_test_part(train_file, test_file, raw_file):
   with open('data/'+train_file, 'w+') as wf_train:
     with open('data/'+test_file, 'w+') as wf_test:
@@ -12,9 +21,15 @@ def train_test_part(train_file, test_file, raw_file):
             wf_train.write(line.strip()+'\n')
 
 if __name__ == '__main__':
-  # token  partition
-  train_token, test_token, raw_token = 'msg_token_train.txt', 'msg_token_test.txt', 'msg_text_token_reshape.txt'
+  # even data distribution
+  raw_file, new_file = 'msg_text_token_reshape.txt', 'msg_text_token_new.txt'
+  data_even(raw_file, new_file)
+  raw_file, new_file = 'msg_label_reshape.txt', 'msg_label_new.txt'
+  data_even(raw_file, new_file)
+  
+  # token partition
+  train_token, test_token, raw_token = 'msg_token_train.txt', 'msg_token_test.txt', 'msg_text_token_new.txt'
   train_test_part(train_token, test_token, raw_token)
   # label train_test partition
-  train_token, test_token, raw_token = 'msg_label_train.txt', 'msg_label_test.txt', 'msg_label_reshape.txt'
+  train_token, test_token, raw_token = 'msg_label_train.txt', 'msg_label_test.txt', 'msg_label_new.txt'
   train_test_part(train_token, test_token, raw_token)
